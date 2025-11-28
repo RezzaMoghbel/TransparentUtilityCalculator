e68551cbbbd0b03968c16a5b8e3910c0f9ed1a6d
Reza Moghbel <r.moghbel@hotmail.com>
2025-11-28 16:26:11 +0000

    Add Email property to User DTO
    - Add Email property to support user email display and management
    - Extends user information available in DTO layer

--- SimpleCRUD.DTO/Identity/User.cs
+++ SimpleCRUD.DTO/Identity/User.cs
@@ -9,6 +9,7 @@ public class User
     public string ID { get; set; } = string.Empty;
 
     public string? UserName { get; set; }
+    public string? Email { get; set; }
 
     public bool EmailConfirmed { get; set; }
 
